3bd914ff9d7d2739e2ae27f6465a60be563a7286
ayesha1906 <memonayesha458@gmail.com>
2020-04-24 12:13:44 +0530

    Delegates example

---
+++ Delegates/Delegates/PhotoProcessor.cs
@@ -0,0 +1,30 @@
+﻿namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo);
+
+        /*
+        public void Process(string path)
+        {
+            var photo = Photo.Load(path);
+            var filters = new PhotoFilters();
+            filters.ApplyBrightness(photo);
+            filters.ApplyContrast(photo);
+            filters.Resize(photo);
+
+            photo.Save();
+        }
+        */
+         // initially we can use the above commented method. But when we use delegate we can do :
+
+        public void Process(string path, PhotoFilterHandler filterHandler)
+        {
+            var photo = Photo.Load(path);
+
+            filterHandler(photo);
+            photo.Save();
+        }
+        
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+
+            //processor.Process("photo1.jpg");
+
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+
+            processor.Process("photo1.jpg", filterHandler);
+        }
+
+        static void RemoveRedEyeFilter (Photo photo)
+        {
+            Console.WriteLine("Apply RemoveRedEye");
+        }
+    }
+}
