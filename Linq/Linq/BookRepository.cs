84dc2ff581e52ff77b8ab76254dab3f2df94730c
ayesha1906 <memonayesha458@gmail.com>
2020-04-28 23:49:20 +0530

    Linq

---
+++ Linq/Linq/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Linq
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book(){Title= "ADO.Net step by step", Price=5},
+                new Book(){Title= "ADO.Net MVC", Price=9.99f},
+                new Book(){Title= "ADO.Net MVC Web API", Price=12},
+                new Book(){Title= "C# Advanced", Price=7},
+                new Book(){Title= "C# Advanced", Price=9}
+
+
+            };
+        }
+    }
+}
---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+            /* Without Linq: 
+            
+            var cheapBooks = new List<Book>();
+            foreach (var b in books)
+            {
+                if (b.Price < 10)
+                    cheapBooks.Add(b);
+            }*/
+
+            //With Linq:
+            //var cheapBooks = books.Where(b => b.Price < 10).OrderBy(b => b.Title);
+            // var ord = cheapBooks.OrderBy(b => b.Title);
+
+            //LINQ Extension Methods
+            var cheapBooks = books
+                                .Where(b => b.Price < 10)
+                                .OrderBy(b => b.Title)
+                                .Select(b => b.Title);         //Select method will turn books into String , so title and price wont be valid:
+
+            foreach (var book in cheapBooks)
+            {
+                //  Console.WriteLine(book.Title+": $" +book.Price);  because of select
+                Console.WriteLine(book);
+            }
+
+            //LINO Query Operators
+            var cheaperBooks = from b in books
+                               where b.Price < 10
+                               orderby b.Title
+                               select b; // then it is Book
+                                         // select b.Ttile; then it is String
+                                         // select b.Price;    then it is float
+
+            foreach (var bk in cheaperBooks)
+                Console.WriteLine(bk.Title+" "+bk.Price);
+
+        }
+    }
+}
