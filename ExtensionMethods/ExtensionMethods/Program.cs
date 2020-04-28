4306ab055cc67f95dc74918e1305e7911eed6537
ayesha1906 <memonayesha458@gmail.com>
2020-04-28 23:48:31 +0530

    Extension

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+        }
+    }
+
+   
+}
---
+++ ExtensionMethods/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class StringExtensions 
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("number of words should be greater or equal to zero");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords))+"...";
+
+        }
+    }
+
+   
+}
