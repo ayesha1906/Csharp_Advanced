8792274b3b0a0b39b2a4a0bf257b8b604168bb48
ayesha1906 <memonayesha458@gmail.com>
2020-05-04 14:29:09 +0530

    Example of Reflections

---
+++ Reflections/Reflections/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Reflections
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer(int ID, string Name)
+        {
+            this.Id = ID;
+            this.Name = Name;
+
+        }
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+
+        }
+
+        public void PrintId()
+        {
+            Console.WriteLine("Id : {0} ", Id);
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Name: {0}",  Name);
+        }
+    }
+}
---
+++ Reflections/Reflections/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflections
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Type T = Type.GetType("Reflections.Customer");
+
+            // Type T = typeof(Customer);
+
+            //We can use this or any of the above commented ways
+
+            Customer c1 = new Customer();
+            Type T = c1.GetType();
+
+
+            Console.WriteLine("Full Name : {0}", T.FullName);
+            Console.WriteLine("Just the Name : {0}", T.Name);
+            Console.WriteLine("Just the Namespace : {0}", T.Namespace);
+
+            Console.WriteLine();
+            Console.WriteLine("About Properties in Customers:");
+            var properties = T.GetProperties(); //here var is of type PropertyInfo[]
+            foreach(var property in properties)
+                Console.WriteLine(property.PropertyType.Name+ " "+property.Name);
+
+            Console.WriteLine();
+            Console.WriteLine("About Methods in Customers:");
+            var methods = T.GetMethods(); //here var is of type MethodInfo[]
+            foreach (var method in methods)
+                Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+
+            Console.WriteLine();
+            Console.WriteLine("About Constructors in Customers:");
+            var constructors = T.GetConstructors(); //here var is of type ConstructorInfo[]
+            foreach (var constructor in constructors)
+            {
+                Console.WriteLine(constructor.Name);
+                Console.WriteLine(constructor.ToString());
+            }
+        }
+    }
+}
